b9b3c5bb27c5c3b308a54892063a21c928842bfe
Przemek Nosek <przemo99.10@gmail.com>
2021-02-21 20:39:22 +0100

    streams are cool

---
+++ src/pl/java/strumienie/CreatingStreams.java
@@ -0,0 +1,68 @@
+package pl.java.strumienie;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class CreatingStreams {
+    public static <T> void show(String title, Stream<T> stream) {
+        final int SIZE = 10;
+        List<T> firstElements =  stream
+                .limit(SIZE +1)
+                .collect(Collectors.toList());
+
+        System.out.print(title + ": ");
+        for (int i = 0; i < firstElements.size(); i++) {
+            if (i > 0) System.out.print(", ");
+            if (i < SIZE) System.out.print(firstElements.get(i));
+            else System.out.print("...");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("alice30.txt");
+        var contets = Files.readString(path);
+
+        Stream<String> words = Stream.of(contets.split("\\PL+"));
+        show("words", words);
+        Stream<String> song = Stream.of("lagodnie", "w", "dol", "strumienia");
+        show("song", song);
+        Stream<String> silence = Stream.empty();
+        show("silence", silence);
+
+        Stream<String> echos = Stream.generate(() -> "Echo");
+        show("echos", echos);
+
+        Stream<Double> randoms = Stream.generate(Math::random);
+        show("randoms", randoms);
+
+        Stream<BigInteger> integers = Stream.iterate(BigInteger.ZERO, n -> n.add(BigInteger.ONE));
+        show("integers", integers);
+
+        Stream<String> wordsAnotherWay = Pattern.compile("\\PL+").splitAsStream(contets);
+        show("wordsAnotherWay", wordsAnotherWay);
+
+        Stream<String> scannerWay = new Scanner(contets).tokens();
+        show("scannerWay", scannerWay);
+
+        try (Stream<String> lines = Files.lines(path, StandardCharsets.UTF_8)) {
+            show("lines", lines);
+        }
+
+        Iterable<Path> iterable = FileSystems.getDefault().getRootDirectories();
+        Stream<Path> rootDirectories = StreamSupport.stream(iterable.spliterator(), false);
+        show("rootDirectories", rootDirectories);
+
+        Iterator<Path> iterator = Paths.get("/sr/share/dict/words").iterator();
+        Stream<Path> pathComponents = StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator, Spliterator.ORDERED), false);
+        show("pathComponents", pathComponents);
+    }
+}
---
+++ src/pl/java/strumienie/Main.java
@@ -0,0 +1,76 @@
+package pl.java.strumienie;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        var contents = Files.readString(Paths.get("alice30.txt"));
+//        List<String> words = List.of(contents.split("\\PL+")); //separator: wszystkie znaki inne niz litery
+//        Instant start = Instant.now();
+//        long count1 = words.stream()
+//                .filter(w -> w.length() > 12)
+//                .sorted(String::compareToIgnoreCase)
+//                .map(String::toUpperCase)
+//                .peek(System.out::println)
+//                .count();
+//        Instant end = Instant.now();
+//        System.out.println(Duration.between(start, end).toMillis());
+//
+//        start = Instant.now();
+//        long count2 = words.parallelStream()
+//                .filter(w -> w.length() > 12)
+//                .count();
+//        end = Instant.now();
+//        System.out.println(Duration.between(start, end).toMillis());
+//
+//        start = Instant.now();
+//        long count3 = 0;
+//        for (String word : words) {
+//            if (word.length() > 12)
+//                count3++;
+//        }
+//        end = Instant.now();
+//        System.out.println(Duration.between(start, end).toMillis());
+//        System.out.println(count1 + " " + count2 + " " + count3);
+
+//        start = Instant.now();
+//        words.forEach(System.out::println);
+//        end = Instant.now();
+//        System.out.println(Duration.between(start, end).toMillis());
+
+
+        //----------------------------------------------------------------------------
+        // 1.2 Tworzenie strumieni, tablice
+        var contents = Files.readString(Paths.get("alice30.txt"));
+        Stream<String> words = Stream.of(contents.split("\\PL+"));
+        words.filter(w -> w.length() > 12)
+                .sorted(String::compareToIgnoreCase);
+
+
+        String[] tab = {"a", "b", "c", "e"};
+        Stream<String> song =
+                Arrays.stream(tab,0,3)
+                .filter(w -> w.length() == 1)
+                .map(String::toUpperCase);
+
+        song.forEach(System.out::println);
+
+/*        Stream<Double> echos = Stream.generate(Math::random)
+        echos.forEach(System.out::println);*/
+
+        var limit = new BigInteger("100");
+        Stream<BigInteger> integers
+                = Stream.iterate(BigInteger.ZERO,
+                n -> n.compareTo(limit) < 0,
+                n -> n.add(BigInteger.ONE));
+        integers.forEach(System.out::println);
+    }
+}
